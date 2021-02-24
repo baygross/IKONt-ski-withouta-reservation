020553051c78f14333ff8f3b3dc59411e8e6e404
Bay Gross <bgross17@gmail.com>
2021-02-23 21:29:12 -0800

    adding dummy creds file

---
+++ creds.rb
@@ -0,0 +1,9 @@
+RESORT= "WINTER PARK RESORT"
+
+SKICREW = [
+#	{
+#		email: "your_email@gmail.com",
+#		password: "yourpass",
+#		dates: ["MARCH 13", "MARCH 20"]  # each string must be in format "MARCH 14". And in order! 
+#		
+]

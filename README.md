12cf2146fa2db0666852575f280aed69e57bd07a
Bay Gross <bgross17@gmail.com>
2021-02-23 21:26:36 -0800

    woohoo

--- README.md
+++ README.md
@@ -1,32 +1,15 @@
-# Shred Bot
+# IKONt ski without a reservation
 
 
-Ski resorts now require reservations to be made in advance of skiing to prevent the spread of Covid. Making these reservations often gets competitive as everyone tries to go all on the same day. People are always cancelling and re-booking to get their spot. I found it tedious always having to check the Ikon website to see if any days had opened up at my resort.
-
-Outside of the annoyance that comes with making ski reservations I decided I wanted to learn more about using Selenium WebDriver in applications.  I've used it at work for feature tests but honestly didn't quite understand what was going on when I got stuck.
+Ski resorts now require reservations to be made in advance of skiing to prevent the spread of Covid. Making these reservations often gets competitive as everyone tries to go all on the same day. People are always cancelling and re-booking to get their spot. Time for a selenium script!
 
 Note: This script only works with resorts that allow reservations to be made directly on the Ikon site.
 
 ---
 
-## Usage
+With original fork from https://github.com/coltonc3/shred-bot
 
-The goal of this project was to make the script run at time intervals specified by the user. This can be done using a Cron job.  If you're unfamiliar with Cron jobs you can learn about them [here](https://ostechnix.com/a-beginners-guide-to-cron-jobs/).    
+---
 
-You must install [WebDriver binaries](https://www.selenium.dev/documentation/en/selenium_installation/installing_webdriver_binaries/) on your machine before anything else.   
+Now supports multiple users, multiple dates. Fixes some edge cases and tightens fail modes. 
 
-Change to the project directory and run `bundle install` to install dependencies. Then create a file called `creds.rb` with the following information for logging into your Ikon account and choosing a mountain:  
-```
-IKON_EMAIL="your.email@example.com"
-IKON_PW="your-password"
-RESORT="your favorite mountain"
-MONTH="month"
-DAY="#"
-```
-Save the file, open Terminal and do the following:  
-`crontab -e`    
-On a new line in the file, enter      
-`*/30 * * * * ruby your/path/to/shred_bot.rb` (this runs the script every 30 mins, change to whatever you want)       
-Allow the crontab to be installed. Then run `crontab -l` to make sure it saved properly. This should show all your Cron jobs.  
- 
-Set-up finished. 
--- shred_bot.rb
+++ shred_bot.rb
@@ -1,100 +1,150 @@
+require 'webdrivers'
 require 'selenium-webdriver'
 require './creds'
 
-"""
-OBJECTIVES: 
-  1. automate ski resort reservation booking
-  2. learn about Selenium & webdrivers
-  3. write maintainable and reusable code for others to use
-"""
 
+## Initialize our globals
 Selenium::WebDriver.logger.level = :debug
-Selenium::WebDriver.logger.output = 'selenium.log'
-driver = Selenium::WebDriver.for :chrome
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+Selenium::WebDriver.logger.output = '/dev/null' # send this to a local file if you want to debug
+DRIVER = Selenium::WebDriver.for :chrome
+WAIT = Selenium::WebDriver::Wait.new(:timeout => 15)
+DRIVER.manage.new_window(:window)
 
-begin
-  Selenium::WebDriver::Chrome.driver_path = '/opt/WebDriver/bin/chromedriver'
 
-  driver.manage.new_window(:window)
+def logout
+  # they have terrible logout click path so just pulling the plug on cookies instead
+  DRIVER.manage.delete_all_cookies
+  puts "☠️ \tLogged out"
+  return
+end
 
+def login(skier)  
   # navigate to ikon account page
-  driver.get 'https://account.ikonpass.com/en/login'
-
-
-
-  # ***** Login Page *****
-  email_box = driver.find_element(id: 'email')
-  email_box.send_keys(IKON_EMAIL)
-  password_box = driver.find_element(id: 'sign-in-password')
-  password_box.send_keys(IKON_PW, :return)
-  puts "✅ \tLogged in"
-
-
+  DRIVER.get 'https://account.ikonpass.com/en/login'
+sleep(1)
   
+  # ***** Login Page *****
+  email_box = DRIVER.find_element(id: 'email')
+  email_box.send_keys(skier[:email])
+  password_box = DRIVER.find_element(id: 'sign-in-password')
+  password_box.send_keys(skier[:password], :return)
+  puts "👨‍💻 \tLogged in as " + skier[:email] + " *hack hack hack hack hack*"
+  
+
   # ***** Account Page *****
   # use an explicit wait to avoid 'no such element' error while loading account page
-  res_button = wait.until{ driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[1]/div/div[1]/div/a') } # Make a Reservation
-  res_button.click
-
+  res_button = WAIT.until{ DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[1]/div/div[1]/div/a') } # Make a Reservation
+  res_button.click  
 
 
   # ***** Resort Selector Page *****
-  search_bar = wait.until{ driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[2]/div[1]/div[1]/div/div/div[1]/input') }
+  search_bar = WAIT.until{ DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[2]/div[1]/div[1]/div/div/div[1]/input') }
   search_bar.send_keys(RESORT)
   search_bar.send_keys(:arrow_down)
   search_bar.send_keys(:return)
-  driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[2]/div[2]/button').click # Continue
-
-
-
-  # ***** Date Selector Page *****
-  available = false
-  sleep(1) # for some reason the explicit wait doesn't work here so use sleep
-  # all available dates are of the class DayPicker-Day so iterate through all of them until specified date is found
-  day_picker = wait.until{ driver.find_elements(class: 'DayPicker-Day') }
-
-  # select date and check availability
-  for date in day_picker
-    if date.text.eql? DAY
-      date.click
-      begin
-        driver.find_element(xpath: "//h2[text() = 'No Reservations Available']")
-      rescue => exception
-        available = true
-      end
-      break
-    end
-  end
-
-  if !available
-    puts "❌ \tspecified date unavailable, try again later"
-    return 
-  else
-    puts "✅ \tspecified date available"
-  end
-
-  # save and continue
-  begin
-    save = driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[1]/div[2]/div/div[4]/button[1]') # Save
-    save.click
-  rescue => exception
-    # save button didn't show up...happens sometimes for unknown reason
-  end
-  continue = wait.until{ driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[2]/button') } # Continue to Confirm
-  continue.click
+  DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[2]/div[2]/button').click # Continue
+  sleep(2)
   
+  puts "🚠 \tLoaded up " + RESORT
+  
+  
+end
 
 
-  # ***** Confirmation Page *****
-  driver.find_element(class: 'input').click # click confirmation checkbox
-  sleep(1)
-  confirm = wait.until{ driver.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[4]/div/div[5]/button/span') } # Confirm Reservations
-  confirm.click
-  sleep(1)
-  puts "✅ \treservations confirmed!"
+def checkDates(dates)
+  
+  for date, date_idx in dates 
+  
+    target_month = date.split(" ")[0]
+    target_day = date.split(" ")[1]
+    
+    # ***** Month Selector Page *****
+    month = DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[1]/div[1]/div[1]/div/div[1]/div[2]/span')
+    until month.text.include? target_month
+      DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[1]/div[1]/div[1]/div/div[1]/div[2]/button[2]').click
+    end
+    
+    # ***** Date Selector Page *****
+    available = false
+    sleep(1) # for some reason the explicit wait doesn't work here so use sleep
+    # all available dates are of the class DayPicker-Day so iterate through all of them until specified date is found
+    day_picker = WAIT.until{ DRIVER.find_elements(class: 'DayPicker-Day') }
+    
+    # select date and check availability
+    for day in day_picker
+      if day.text.eql? target_day
+        day.click
+        
+        # oh boy forgive me this is gnarly. have to check status of that day now
+        x = DRIVER.find_element(xpath: "//h2[text() = 'No Reservations Available']") rescue nil
+        if x.nil? 
+          x = DRIVER.find_element(xpath: "//h2[text() = 'Reservation Details']") rescue nil 
+          if x.nil? 
+            puts "🔥 \t" + date + " specified date available. Hell ya!!!"
+            available = true
+          else 
+            puts "🤔 \t" + date + " lol we already have that date reserved"  
+          end 
+        else
+          puts "😭 \t" + date + " unavailable, try again later"
+        end
+        
+      end
+    end
+    
+    next if !available # move on to next date in choices if this one was dead
+    
+    # save and continue
+    begin
+      save = DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[1]/div[2]/div/div[3]/button[1]') # Save
+      save.click
+    rescue => exception
+      puts "🤷‍♂️ \tsave button wasn't there idk gave up"
+      next 
+    end
+    continue = WAIT.until{ DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[3]/div[2]/button') } # Continue to Confirm
+    continue.click
+    
+  
+    # ***** Confirmation Page *****
+    DRIVER.find_element(class: 'input').click # click confirmation checkbox
+    sleep(1)
+    confirm = WAIT.until{ DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div[2]/div[4]/div/div[5]/button/span') } # Confirm Reservations
+    confirm.click
+    sleep(1)
+    puts "✅ \treservation confirmed!"
+    dates.delete_at(date_idx)
+  
+  
+    # Success! Now lets go back to make another reservation
+    confirm = WAIT.until{ DRIVER.find_element(xpath: '//*[@id="root"]/div/div/main/section[2]/div/div/div[3]/a[2]') } 
+    confirm.click
+    # now you are back on the date select page.. still on same resort
+  end
+  
+  puts "👋 \tall dates have been tried!"
+  
+end
 
-  #TODO: some success notification (already get an email from Ikon, maybe I can do a text??)
+
+
+begin
+  
+  while !SKICREW.empty?
+  
+    for skier, skier_idx in SKICREW
+      login(skier)
+      checkDates(skier[:dates])
+      logout
+      
+      SKICREW.delete_at(skier_idx) if skier[:dates].empty? 
+      puts "😴 \tsleeping for a minute to throw off the 👮‍♀️"
+      sleep(60)  # wait a minute before logging in as next person
+    end
+    
+    puts "😴 \tsleeping for 15 minutes to throw off the 👮‍♀️"
+    sleep(15 * 60) # wait 20 minutes before trying again
+  end
 ensure
-  driver.quit
+  DRIVER.quit
 end
